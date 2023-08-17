c822c525c1b77ff01006e46b5015c1bebf48fd90
KenthJohan <johan.soderlind.astrom@gmail.com>
2023-08-17 22:02:57 +0200

    update

--- lidar_scan_example/include/lidar_scan_example/bake_config.h
+++ lidar_scan_example/include/lidar_scan_example/bake_config.h
@@ -14,8 +14,8 @@
  * dependencies will automatically show up in this file. Include bake_config.h
  * in your main project file. Do not edit! */
 
-#ifndef APP1_BAKE_CONFIG_H
-#define APP1_BAKE_CONFIG_H
+#ifndef LIDAR_SCAN_EXAMPLE_BAKE_CONFIG_H
+#define LIDAR_SCAN_EXAMPLE_BAKE_CONFIG_H
 
 /* Headers of public dependencies */
 /* No dependencies */

cb2eab89a12d05fa5a4c55ba3065114dfaca25fc
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-10-13 08:40:37 +0200

    update

--- tools_install.sh
+++ tools_install.sh
@@ -10,10 +10,10 @@ set -x #echo on
 
 
 bake rebuild -r --cfg release tools
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_capture1 /usr/local/bin/
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_replay1 /usr/local/bin/
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_snapshot /usr/local/bin/
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_ether /usr/local/bin/
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_meta /usr/local/bin/
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_monitor /usr/local/bin/
-sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_view /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_capture1 /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_replay1 /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_snapshot /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_ether /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_meta /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_monitor /usr/local/bin/
+sudo cp $HOME/bake/x64-Linux/release/bin/ouster_view /usr/local/bin/

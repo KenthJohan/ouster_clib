e2286a6e21a0afee5e8372bb13f6dbd3ccb35645
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-10-13 08:27:35 +0200

    update

--- tools_install.sh
+++ tools_install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x #echo on
 
+# ouster_ether REQUIRES
+# sudo apt install libpcap-dev
+
+# ouster_view REQUIRES
+# sudo apt-get install build-essential libgl1-mesa-dev
+# sudo apt install libx11-dev
+
+
 bake rebuild -r --cfg release tools
 sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_capture1 /usr/local/bin/
 sudo cp /home/johan/bake/x64-Linux/release/bin/ouster_replay1 /usr/local/bin/

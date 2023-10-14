8abee016dca5b830225eda935a676cf45b7f58b8
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-10-14 14:11:11 +0200

    update

--- ouster_clib/include/ouster_clib/ouster_sock.h
+++ ouster_clib/include/ouster_clib/ouster_sock.h
@@ -1,4 +1,13 @@
-#pragma once
+/**
+ * @defgroup sock UDP Capture
+ * @brief This creates sockets
+ * 
+ * \ingroup ouster_sock
+ * @{
+ */
+
+#ifndef OUSTER_SOCK_H
+#define OUSTER_SOCK_H
 
 #ifdef __cplusplus
 extern "C"
@@ -14,3 +23,7 @@ int ouster_sock_create_tcp(char const *hint_name);
 #ifdef __cplusplus
 }
 #endif
+
+#endif // OUSTER_SOCK_H
+
+/** @} */

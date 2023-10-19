0fd9e42cbd185e70dcb526aa5ac38dddfacbdcc4
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-10-19 17:21:30 +0200

    update

--- tools/ouster_replay1/src/ouster_replay.c
+++ tools/ouster_replay1/src/ouster_replay.c
@@ -9,6 +9,9 @@
 #include <unistd.h>
 
 #include <ouster_clib.h>
+
+#include "argparse.h"
+
 typedef struct
 {
 	char const *metafile;
@@ -16,52 +19,71 @@ typedef struct
 	char const *ip_dst;
 	FILE *read_file;
 	ouster_meta_t meta;
+	int offset;
 } app_t;
 
-void print_help(int argc, char *argv[])
-{
-	printf("Hello welcome to %s!\n", "ouster_replay1");
-	printf("This tool replays custom UDP capture file.\n");
-	printf("Arguments:\n");
-	printf("\targ1: The meta file that correspond to the LiDAR sensor configuration\n");
-	printf("\targ2: The capture file to replay\n");
-	printf("\targ3: The destination ip address. (Optional, default=127.0.0.1)\n");
-	printf("Examples:\n");
-	printf("\t$ %s <%s> <%s> <%s>\n", argv[0], "arg1", "arg2", "arg3");
-	printf("\t$ %s <%s> <%s> <%s>\n", argv[0], "matafile", "capturefile", "ip");
-	printf("\t$ %s %s %s %s\n", argv[0], "meta.json", "capture.udpcap", "127.0.0.1");
-	printf("\t$ %s %s %s\n", argv[0], "meta.json", "capture.udpcap");
-}
+static const char *const usages[] = {
+    "ouster_replay1 [options] [[--] args]",
+    "ouster_replay1 [options]",
+    NULL,
+};
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("==================ouster_replay1================================\n");
+	printf("ouster_replay1================================\n");
 	ouster_fs_pwd();
 
-	app_t app = {0};
+	app_t app = {
+	    .ip_dst = "127.0.0.1",
+	    .offset = 0};
 
-	if (argc == 3) {
-		app.metafile = argv[1];
-		app.read_filename = argv[2];
-		app.ip_dst = "127.0.0.1";
-		printf("Command: %s %s %s\n", argv[0], argv[1], argv[2]);
-	} else if (argc == 4) {
-		app.metafile = argv[1];
-		app.read_filename = argv[2];
-		app.ip_dst = argv[3];
-		printf("Command: %s %s %s %s\n", argv[0], argv[1], argv[2], argv[3]);
-	} else {
-		print_help(argc, argv);
+	struct argparse_option options[] = {
+	    OPT_HELP(),
+	    OPT_GROUP("Basic options"),
+	    OPT_STRING('m', "metafile", &app.metafile, "The meta file that correspond to the LiDAR sensor configuration", NULL, 0, 0),
+	    OPT_STRING('c', "capture", &app.read_filename, "The capture file to replay", NULL, 0, 0),
+	    OPT_STRING('d', "destination", &app.ip_dst, "The destination ip address. (Optional, default=127.0.0.1)", NULL, 0, 0),
+	    OPT_INTEGER('o', "offset", &app.offset, "Where to start replay", NULL, 0, 0),
+	    OPT_END(),
+	};
+
+	struct argparse argparse;
+	argparse_init(&argparse, options, usages, 0);
+	argparse_describe(&argparse, "\nA brief description of what the program does and how it works.", "\nAdditional description of the program after the description of the arguments.");
+	argc = argparse_parse(&argparse, argc, argv);
+
+	if (app.metafile == NULL) {
+		argparse_usage(&argparse);
+		return -1;
+	}
+
+	if (app.ip_dst == NULL) {
+		argparse_usage(&argparse);
+		return -1;
+	}
+
+	if (app.read_filename == NULL) {
+		argparse_usage(&argparse);
+		return -1;
 	}
 
 	ouster_log("Opening file '%s'\n", app.read_filename);
+
 	app.read_file = fopen(app.read_filename, "r");
-	ouster_assert_notnull(app.read_file);
+	if (app.read_file == NULL) {
+		char buf[1024];
+		ouster_fs_readfile_failed_reason(app.read_filename, buf, 1024);
+		fprintf(stderr, "%s", buf);
+		return -1;
+	}
 
 	{
 		char *content = ouster_fs_readfile(app.metafile);
 		if (content == NULL) {
-			return 0;
+			char buf[1024];
+			ouster_fs_readfile_failed_reason(app.read_filename, buf, 1024);
+			fprintf(stderr, "%s", buf);
+			return -1;
 		}
 		ouster_meta_parse(content, &app.meta);
 		free(content);
@@ -78,6 +100,17 @@ int main(int argc, char *argv[])
 
 	uint32_t packet_id = 0;
 	ouster_udpcap_t *cap = calloc(1, sizeof(ouster_udpcap_t) + OUSTER_NET_UDP_MAX_SIZE);
+
+	for (int i = 0; i < app.offset; ++i) {
+		cap->size = OUSTER_NET_UDP_MAX_SIZE;
+		int nread = ouster_udpcap_read(cap, app.read_file);
+		if (nread != OUSTER_UDPCAP_OK) {
+			fprintf(stderr, "error: ouster_udpcap_read: %i\n", nread);
+			return -1;
+		}
+		packet_id++;
+	}
+
 	while (1) {
 		cap->size = OUSTER_NET_UDP_MAX_SIZE;
 

25c94a3a2fc12366b84510ba51c5619a3f21b449
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-12-14 11:28:07 +0100

    init

---
+++ src/ouster_os_api.c
@@ -0,0 +1,30 @@
+#include "ouster_clib/ouster_os_api.h"
+
+ouster_os_api_t ouster_os_api;
+
+
+
+void ouster_os_dbg(const char *file, int32_t line, const char *msg)
+{
+
+}
+
+void ouster_os_trace(const char *file, int32_t line, const char *msg)
+{
+
+}
+
+void ouster_os_warn(const char *file, int32_t line, const char *msg)
+{
+
+}
+
+void ouster_os_err(const char *file, int32_t line, const char *msg)
+{
+
+}
+
+void ouster_os_fatal(const char *file, int32_t line, const char *msg)
+{
+
+}
